87b052b530aa24bb177d2ec1b6aeeee0910f241e
Stan Majerski <stan.majerski@gmail.com>
2018-05-23 11:14:22 +0100

    Modified icon handlebar helper

--- src/helpers/icon.js
+++ src/helpers/icon.js
@@ -1,7 +1,11 @@
 const Handlebars = require('handlebars');
 
-module.exports = function(url, icon) {    
-    var result = '<a href="' + url + '"><img src="assets/img/icons/' + icon + '.png" alt="' + icon + '"></a>';
+module.exports = function(network, url, style) {
+    
+    var result = '<a href="' + 
+    url + '" class="social-icon"><img src="assets/img/social-icons/' + 
+    network + '-' + style + '.png" alt="' + 
+    network + '" width="36" height="36"></a>&nbsp;';
     
     return new Handlebars.SafeString(result);
 }
